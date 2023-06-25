5c4a383c54832caa540ecbc8cf2d249adb05b7e0
Marvin <marvinnrn@gmail.com>
2023-06-25 05:38:33 +0200

    Initialisation du projet

---
+++ my-portfolio/app/page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import RootLayout from './layout';
+
+const mainPage = () => {
+  return (
+    <RootLayout>
+      <h1>Hello World</h1>
+    </RootLayout>
+  );
+};
+
+export default mainPage;
